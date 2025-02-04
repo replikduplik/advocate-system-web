28709b2d683f832c78abff59218cfd3a50f99ff1
replikduplik <3cagdas3@gmail.com>
2025-02-04 11:22:42 +0300

    shadcn card ve switch comments

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 export default function SignInPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return <div></div>;
 }
+
+// shadcn.com card component eklenecek ve form oluşturulacak.
+// shadcn.com switch component eklenecek.
+
+//htmlFor elementi label etiketine eklenecek.
+
