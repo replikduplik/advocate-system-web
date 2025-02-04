39fa9686a44b83dad85de2443683b13b971f31f9
replikduplik <3cagdas3@gmail.com>
2025-02-04 11:23:01 +0300

    button ve input add

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
 
 export default function Navbar() {
+	const [isLoginOpen, setIsLoginOpen] = useState(false);
+
 	return (
 		<nav className="flex justify-between p-5">
 			<ul className="flex gap-6">
 				<li>
 					<Link href="/">Home</Link>
-				</li> 
+				</li>
 				<li>
 					<Link href="/about-us">Hakkımızda</Link>
 				</li>
@@ -16,9 +23,30 @@ export default function Navbar() {
 			</ul>
 			<ul>
 				<li>
-					<Link href="/auth/sign-in">Giriş</Link>
+					<Button onClick={() => setIsLoginOpen(true)}>Giriş</Button>
 				</li>
 			</ul>
+
+			{isLoginOpen && (
+				<div className= "absolute inset-0 flex justify-center items-center bg-black/20">
+					<div className="popup-overlay border-2 bg-red-400 p-8 rounded-lg">
+						<div className="popup-box">
+							<button
+								onClick={() => setIsLoginOpen(false)}
+								className="close-button"
+							>
+								&times;
+							</button>
+							<h2>Giriş Yap</h2>
+							<form>
+								<Input  type="text" placeholder="Kullanıcı Adı" className='bg-white'/>
+								<Input type="password" placeholder="Şifre" />
+								<Button type="submit">Giriş Yap</Button>
+							</form>
+						</div>
+					</div>
+				</div>
+			)}
 		</nav>
 	);
 }
