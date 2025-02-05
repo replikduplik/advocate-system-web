03562a42aa3dccf600537e9229c948cf6b5a2ca7
replikduplik <3cagdas3@gmail.com>
2025-02-05 22:20:48 +0300

    card layout sign in add

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,9 +1,33 @@
 import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardContent,
+	CardTitle,
+	CardFooter,
+	CardHeader,
+} from '@/components/ui/card';
+
+import { Input } from '@/components/ui/input';
+import { Switch } from '@/components/ui/switch';
+
 export default function SignInPage() {
-	return <div></div>;
+	return (
+		<Card className="w-80 mx-auto mt-14">
+			<CardHeader>
+				<CardTitle>Sign In</CardTitle>
+			</CardHeader>
+			<CardContent className="flex flex-col gap-4">
+				<Input placeholder="Kullanıcı Adı veya E-posta" />
+				<Input placeholder="Şifre" />
+				<div className="flex items-center justify-center gap-2">
+					<label htmlFor="accountType">Bireysel</label>
+					<Switch id="accountType" />
+					<label htmlFor="accountType">Kurumsal</label>
+				</div>
+			</CardContent>
+			<CardFooter>
+				<Button className="w-full">Giriş Yap</Button>
+			</CardFooter>
+		</Card>
+	);
 }
-
-// shadcn.com card component eklenecek ve form oluşturulacak.
-// shadcn.com switch component eklenecek.
-
-//htmlFor elementi label etiketine eklenecek.
